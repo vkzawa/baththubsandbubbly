5328c7b3a25a4a7a92fc3f1b9ee0ca6360d9762b
Vince Kurzawa <vince@creativengine.com>
2021-03-15 01:15:18 -0700

    Add plugin gatsby-cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         minify: false, // Breaks styles if not set to false
